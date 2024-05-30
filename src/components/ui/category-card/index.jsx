17a52347d4184e17c5e7163ee56bc72bc0a1aacf
Otabek <azamov521o@gmail.com>
2024-05-30 11:48:15 +0500

    completed

--- src/components/ui/category-card/index.jsx
+++ src/components/ui/category-card/index.jsx
@@ -15,7 +15,7 @@ function index({data}) {
 
   return (
     <div onClick={() => getData(data.id)} className='w-[220px] h-[90px] bg-[#ffffff26] py-[14px] px-[20px] flex items-center gap-[12px] cursor-pointer rounded-xl'>
-       <img src={data?.image || 'https://restaurant-crm-pvt3.vercel.app/assets/noimage-IR2Iw3UG.png'} alt="IMG" className='w-[62px] h-[62px] object-cover'/>
+       <img src={data?.image || 'https://restaurant-crm-pvt3.vercel.app/assets/noimage-IR2Iw3UG.png'} alt="IMG" className='w-[62px] h-[62px] object-cover rounded-[50%] '/>
         <h2 className='font-bold text-[20px] capitalize'>{data?.name}</h2>
     </div>
   )
